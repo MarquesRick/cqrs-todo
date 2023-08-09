52b8e53e098007ec93505af71b7e2c64245fdc76
marques-rick <hmservicostech@outlook.com.br>
2023-08-09 20:04:33 -0300

    created todoitementity and domain actions

---
+++ Todo.Domain/Entities/TodoItemEntity.cs
@@ -0,0 +1,35 @@
+namespace Todo.Domain.Entities
+{
+    public class TodoItemEntity : Entity
+    {
+        public TodoItemEntity(string title, DateTime date, string user)
+        {
+            Title = title;
+            Done = false;
+            Date = date;
+            User = user;
+        }
+
+        public string Title { get; private set; }
+        public bool Done { get; private set; }
+        public DateTime Date { get; private set; }
+
+        //Authentication from google
+        public string User { get; private set; }
+
+        public void MarkAsDone()
+        {
+            Done = true;
+        }
+
+        public void MarkAsUndone()
+        {
+            Done = false;
+        }
+
+        public void UpdateTitle(string title)
+        {
+            Title = title;
+        }
+    }
+}
