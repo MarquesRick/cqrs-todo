893f6828da08b7179eb2f402db50f5b0966cb5cc
marques-rick <hmservicostech@outlook.com.br>
2023-08-12 14:13:12 -0300

    create reposotory implementations

--- Todo.Domain.Infra/Repositories/TodoRepository.cs
+++ Todo.Domain.Infra/Repositories/TodoRepository.cs
@@ -1,43 +1,59 @@
+using Microsoft.EntityFrameworkCore;
 using Todo.Domain.Entities;
+using Todo.Domain.Infra.Contexts;
+using Todo.Domain.Queries;
 using Todo.Domain.Repositories;
 
 namespace Todo.Domain.Infra.Repositories
 {
     public class TodoRepository : ITodoRepository
     {
+        private readonly SQLiteContext _context;
+
+        public TodoRepository(SQLiteContext context)
+        {
+            _context = context;
+        }
+
         public void Create(TodoItemEntity todo)
         {
-            throw new NotImplementedException();
+            _context.Add(todo);
+            _context.SaveChanges();
         }
 
         public IEnumerable<TodoItemEntity> GetAll(string user)
-        {
-            throw new NotImplementedException();
-        }
+            => _context.Todos
+                .AsNoTracking()
+                .Where(TodoQueries.GetAll(user))
+                .OrderBy(x => x.Date);
 
         public IEnumerable<TodoItemEntity> GetAllDone(string user)
-        {
-            throw new NotImplementedException();
-        }
+            => _context.Todos
+                .AsNoTracking()
+                .Where(TodoQueries.GetAllDone(user))
+                .OrderBy(x => x.Date);
 
         public IEnumerable<TodoItemEntity> GetAllUndone(string user)
-        {
-            throw new NotImplementedException();
-        }
+            => _context.Todos
+                .AsNoTracking()
+                .Where(TodoQueries.GetAllUndone(user))
+                .OrderBy(x => x.Date);
 
         public TodoItemEntity GetById(Guid id, string user)
-        {
-            throw new NotImplementedException();
-        }
+            => _context.Todos
+                .FirstOrDefault(x => x.Id == id && x.User == user);
 
         public IEnumerable<TodoItemEntity> GetByPeriod(string user, DateTime date, bool done)
-        {
-            throw new NotImplementedException();
-        }
+            => _context.Todos
+                .AsNoTracking()
+                .Where(TodoQueries.GetByPeriod(user, date, done))
+                .OrderBy(x => x.Date);
+
 
         public void Update(TodoItemEntity todo)
         {
-            throw new NotImplementedException();
+            _context.Entry(todo).State = EntityState.Modified;
+            _context.SaveChanges();
         }
     }
 }
