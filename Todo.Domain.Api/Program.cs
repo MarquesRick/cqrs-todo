322fd76d2b78fc3b9049d983731c1ac0df697356
marques-rick <hmservicostech@outlook.com.br>
2023-08-12 16:14:39 -0300

    created get uris/methods

--- Todo.Domain.Api/Program.cs
+++ Todo.Domain.Api/Program.cs
@@ -24,17 +24,45 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.MapGet("/v1/todos/getAll", ([FromServices]ITodoRepository repository)=>
+# region Gets
+app.MapGet("/v1/todos/all", ([FromServices]ITodoRepository repository)=>
 {
     return repository.GetAll("Henrique");
 });
+app.MapGet("/v1/todos/allDone", ([FromServices] ITodoRepository repository) =>
+{
+    return repository.GetAllDone("Henrique");
+});
+app.MapGet("/v1/todos/allUndone", ([FromServices] ITodoRepository repository) =>
+{
+    return repository.GetAllUndone("Henrique");
+});
+app.MapGet("/v1/todos/done/today", ([FromServices] ITodoRepository repository) =>
+{
+    return repository.GetByPeriod("Henrique", DateTime.Now.Date, true);
+});
+app.MapGet("/v1/todos/undone/today", ([FromServices] ITodoRepository repository) =>
+{
+    return repository.GetByPeriod("Henrique", DateTime.Now.Date, false);
+});
+app.MapGet("/v1/todos/done/tomorrow", ([FromServices] ITodoRepository repository) =>
+{
+    return repository.GetByPeriod("Henrique", DateTime.Now.Date.AddDays(1), true);
+});
+app.MapGet("/v1/todos/undone/tomorrow", ([FromServices] ITodoRepository repository) =>
+{
+    return repository.GetByPeriod("Henrique", DateTime.Now.Date.AddDays(1), false);
+});
+#endregion
 
+# region Commands
 app.MapPost("v1/todos/create", ([FromBody]CreateTodoCommand command, [FromServices]TodoHandler handler)=>
 {
     //command.User = User.Claims.FirstOrDefault(x => x.Type == "user_id")?.Value;
     command.User = "Henrique";
     return (GenericCommandResult)handler.Handle(command);
 });
+#endregion
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
