5eeae52517973266468a439c548c4fac3365536e
marques-rick <hmservicostech@outlook.com.br>
2023-08-13 18:35:09 -0300

    created respository fake and handler test

---
+++ Todo.Domain.Tests/HandlerTests/CreateTodoHandlerTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Todo.Domain.Commands;
+using Todo.Domain.Handlers;
+using Todo.Domain.Tests.Repositories;
+
+namespace Todo.Domain.Tests.HandlerTests
+{
+    [TestClass]
+    public class CreateTodoHandlerTest
+    {
+        private readonly CreateTodoCommand _invalidCommand = new CreateTodoCommand("", "", DateTime.Now);
+        private readonly CreateTodoCommand _validCommand = new CreateTodoCommand("Title Here", "Henrique", DateTime.Now);
+        private readonly TodoHandler _handler = new TodoHandler(new FakeTodoRepository());
+        private GenericCommandResult _result = new GenericCommandResult();
+
+        public CreateTodoHandlerTest() { }
+
+        [TestMethod]
+        public void Give_Wrong_Data_Fail_Execution()
+        {
+            _result = (GenericCommandResult)_handler.Handle(_invalidCommand);
+            Assert.AreEqual(_result.Success, false);
+        }
+
+        [TestMethod]
+        public void Give_Right_Data_Ok_Execution()
+        {
+            _result = (GenericCommandResult)_handler.Handle(_validCommand);
+            Assert.AreEqual(_result.Success, true);
+        }
+    }
+}
+
